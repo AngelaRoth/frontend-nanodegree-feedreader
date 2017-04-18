2bc73e6a48f88e54dea003948911cae745e4f006
Angela <Angela>
2017-04-18 16:31:25 -0400

    Specifically check for Children of Class "entry"

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -91,7 +91,19 @@ $(function() {
 
 
     /* Step 13: Write a new test suite named "Initial Entries" */
+    // NOTE: Ideally, this test would check the init()
+    // function, not the loadFeed() function. However,
+    // I want to make use of the callback in loadFeed().
+    // Init() has no such callback and all it really does
+    // is call loadFeed() on the first feed (id 0); so
+    // I am simulating a call to the init() function by
+    // calling the loadFeed() function on the first feed
+    // (id 0)
     describe('Initial Entries', function() {
+        /* Step 14: Write a test that ensures when the loadFeed
+         * function is called and completes its work, there is at least
+         * a single .entry element within the .feed container.
+         */
         beforeEach(function(done) {
             loadFeed(0, function() {
                 done();
@@ -99,18 +111,83 @@ $(function() {
         });
 
         it('exist on load', function(done) {
+/*
             var numFeedChildren = $('.feed').children().length;
             expect(numFeedChildren).toBeGreaterThan(0);
+*/
+            var numDescendantsOfClassEntry = $('.feed .entry').length;
+            expect(numDescendantsOfClassEntry).toBeGreaterThan(0);
             done();
         });
     });
+/*
+    // EXTRA TEST: It bothered me that we are only checking the existance
+    // of entries for the first feed, so I created this function to check
+    // for the existance of entries on any inputted feed ID.
+    function test_for_entries(feedID) {
+        describe('Entries for Feed ' + feedID, function() {
+            beforeEach(function(done) {
+                // Make sure "feed" container is empty of
+                // entries from other feeds
+                $('.feed').empty();
+                loadFeed(feedID, function() {
+                    done();
+                });
+            });
+
+            it('exist', function(done) {
+                var numFeedChildren = $('.feed').children().length;
+                expect(numFeedChildren).toBeGreaterThan(0);
+                done();
+            });
+        });
+    }
+
+    // Test for entries for each feed in allFeeds
+    // Loop backwards through feeds so we finish in our "on load"
+    // state, with results from first feed being displayed
+    var numFeeds = allFeeds.length;
+    for (var i = numFeeds - 1; i >= 0; i--) {
+        console.log('loop # ' + i);
+        test_for_entries(i);
+    }
+
+*/
 
 
 
 
 
 
+/*
+    describe('Initial Entries', function() {
 
+        console.log('numFeedListItems = ' + numFeedListItems);
+
+        function test_if_exist(feedNum) {
+            beforeEach(function(done) {
+                console.log('feedNum = ' + feedNum);
+                loadFeed(feedNum, function() {
+                    done();
+                });
+            });
+
+            it('for feed ' + feedNum + ' exist on load', function(done) {
+                var numFeedChildren = $('.feed').children().length;
+                console.log('numFeedChildren = ' + numFeedChildren + '; feedNum = ' + feedNum);
+                expect(numFeedChildren).toBeTruthy();
+                done();
+            });
+
+        }
+
+        for (var i = 0; i < numFeedListItems; i++) {
+            console.log('in loop ' + i);
+            test_if_exist(i);
+        }
+
+    });
+*/
 
 /*
     describe('Initial Entries', function() {
